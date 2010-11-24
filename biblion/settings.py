30e8b2fb8bb699590a5ed6062d3da3d01067ec5f
Alex Marandon <contact@alexmarandon.com>
2010-11-24 16:14:10 +0700

    Don't expect BIBLION_SECTIONS to be present
    Biblion was expecting the presence of BIBLION_SECTIONS in settings.py.
    Because of this Pinax's test suite was not running at all. With this
    litte change, it can run again (even though there are a a lot of other
    problems with it).

--- biblion/settings.py
+++ biblion/settings.py
@@ -3,4 +3,4 @@ from django.conf import settings
 
 
 ALL_SECTION_NAME = getattr(settings, "BIBLION_ALL_SECTION_NAME", "all")
-SECTIONS = settings.BIBLION_SECTIONS
+SECTIONS = getattr(settings, "BIBLION_SECTIONS", [])
