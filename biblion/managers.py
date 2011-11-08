bdc0176b238b9a4893b393fd33d96d1f4a2373e5
Patrick Altman <paltman@gmail.com>
2011-11-07 18:03:35 -0600

    Exclude future published dates

--- biblion/managers.py
+++ biblion/managers.py
@@ -1,10 +1,16 @@
+import datetime
+
 from django.db import models
 
 
 class PostManager(models.Manager):
     
     def published(self):
-        return self.exclude(published=None)
+        return self.exclude(
+            published=None
+        ).exclude(
+            published__gt=datetime.datetime.now()
+        )
     
     def current(self):
         return self.published().order_by("-published")
