f9b3a10bcb42daab9a76e2f6083e00526460b8e3
Brian Rosner <brosner@gmail.com>
2011-11-14 14:02:47 -0700

    updated templatetags to take biblion

--- biblion/templatetags/biblion_tags.py
+++ biblion/templatetags/biblion_tags.py
@@ -6,31 +6,38 @@ from biblion.models import Post, Section
 register = template.Library()
 
 
-class LatestBlogPostsNode(template.Node):
+class LatestBiblionPostsNode(template.Node):
     
-    def __init__(self, context_var):
+    def __init__(self, biblion, context_var):
+        self.biblion = template.Variable(biblion)
         self.context_var = context_var
     
     def render(self, context):
-        latest_posts = Post.objects.current()[:5]
+        biblion = self.biblion.resolve(context)
+        latest_posts = biblion.posts.current()[:5]
         context[self.context_var] = latest_posts
         return u""
 
 
 @register.tag
-def latest_blog_posts(parser, token):
+def latest_biblion_posts(parser, token):
+    """
+        {% latest_biblion_posts biblion as posts %}
+    """
     bits = token.split_contents()
-    return LatestBlogPostsNode(bits[2])
+    return LatestBiblionPostsNode(bits[1], bits[3])
 
 
-class LatestBlogPostNode(template.Node):
+class LatestBiblionPostNode(template.Node):
     
-    def __init__(self, context_var):
+    def __init__(self, biblion, context_var):
+        self.biblion = template.Variable(biblion)
         self.context_var = context_var
     
     def render(self, context):
+        biblion = self.biblion.resolve(context)
         try:
-            latest_post = Post.objects.current()[0]
+            latest_post = biblion.posts.current()[0]
         except IndexError:
             latest_post = None
         context[self.context_var] = latest_post
@@ -38,6 +45,6 @@ class LatestBlogPostNode(template.Node):
 
 
 @register.tag
-def latest_blog_post(parser, token):
+def latest_biblion_post(parser, token):
     bits = token.split_contents()
-    return LatestBlogPostNode(bits[2])
+    return LatestBiblionPostNode(bits[1], bits[2])
