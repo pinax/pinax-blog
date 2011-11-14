5aef9fb1acd2eea9a9b73a6f16d977e8c02ff3ba
Brian Rosner <brosner@gmail.com>
2011-11-14 11:04:43 -0700

    fixed biblion_detail URL from not matching anything

--- biblion/urls.py
+++ biblion/urls.py
@@ -6,5 +6,5 @@ import biblion.views
 urlpatterns = patterns("",
     url(r"^$", biblion.views.BiblionList.as_view(), name="biblion_list"),
     url(r"^create/$", biblion.views.BiblionCreate.as_view(), name="biblion_create"),
-    url(r"^blog/(?P<slug>[\w-])/$", biblion.views.BiblionDetail.as_view(), name="biblion_detail"),
+    url(r"^blog/(?P<slug>[\w-]+)/$", biblion.views.BiblionDetail.as_view(), name="biblion_detail"),
 )
