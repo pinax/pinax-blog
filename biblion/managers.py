9f07ce5bb178acb95d0effa395c48e90967cfe12
Brian Rosner <brosner@gmail.com>
2011-12-06 08:14:52 -0700

    fixed published bit (worked in previous commit; i changed it and caused the bug)

--- biblion/managers.py
+++ biblion/managers.py
@@ -7,7 +7,8 @@ from django.db import models
 class PostManager(models.Manager):
     
     def published(self):
-        qs = self.exclude(published=None, published__gt=datetime.datetime.now())
+        qs = self.exclude(published=None)
+        qs = qs.exclude(published__gt=datetime.datetime.now())
         #qs = qs.filter(sites=settings.SITE_ID)
         return qs
     
