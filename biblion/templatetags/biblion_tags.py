5eee15b0574f5c7eddb9dfb5a987afa742d35322
rizumu <tom@rizu.mu>
2013-01-24 14:12:24 +0100

    add back the lost show_post_brief templatetag

--- biblion/templatetags/biblion_tags.py
+++ biblion/templatetags/biblion_tags.py
@@ -7,3 +7,13 @@ register = template.Library()
 
 
 register.filter("to_html", to_html)
+
+
+def show_post_brief(context, post):
+    return {
+        "post": post,
+        "last": context["forloop"]["last"],
+        "can_edit": context["user"].is_staff,
+    }
+
+register.inclusion_tag("blog/post_brief.html", takes_context=True)(show_post_brief)
