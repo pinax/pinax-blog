aefa96b8b548a444958ff16ea717bc9b4c9697f9
Patrick Altman <paltman@gmail.com>
2014-01-12 21:51:38 -0600

    Output stats and benchmark

--- ci.sh
+++ ci.sh
@@ -3,6 +3,6 @@ set -e
 export DJANGO_SETTINGS_MODULE=tests.settings
 export PYTHONPATH=.
 
-flake8 biblion --ignore=E501 --max-complexity=10
+flake8 biblion --ignore=E501 --max-complexity=10 --statistics --benchmark
 coverage run --branch --source=biblion `which django-admin.py` test tests
 coverage report
