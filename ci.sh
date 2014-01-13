94ec29ad37f0a2805e5f2d6dc2ce4ca496bf06b6
Patrick Altman <paltman@gmail.com>
2014-01-12 21:37:09 -0600

    Add check for complexity

--- ci.sh
+++ ci.sh
@@ -3,6 +3,6 @@ set -e
 export DJANGO_SETTINGS_MODULE=tests.settings
 export PYTHONPATH=.
 
-flake8 biblion --ignore=E124,E501,E127,E128
+flake8 biblion --ignore=E124,E501,E127,E128 --max-complexity=10
 coverage run --branch --source=biblion `which django-admin.py` test tests
 coverage report
