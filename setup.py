4c23dee55734f81e3fc296eaed5c220f6ba7a99c
Patrick Altman <paltman@gmail.com>
2014-01-24 12:40:50 -0600

    2.1.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from distutils.core import setup
 
 setup(
     name = "biblion",
-    version = "2.0.0",
+    version = "2.1.0",
     author = "Eldarion",
     author_email = "development@eldarion.com",
     description = "the eldarion.com blog app intended to be suitable for site-level company and project blogs",
