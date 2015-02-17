34bb6b5877c490d067c3a34b65ed1d699ba14c26
Patrick Altman <paltman@gmail.com>
2015-02-16 21:22:19 -0600

    3.0.1

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ URL = "https://github.com/pinax/pinax-blog"
 
 setup(
     name=NAME,
-    version="3.0.0",
+    version="3.0.1",
     description=DESCRIPTION,
     long_description=read("README.rst"),
     url=URL,
