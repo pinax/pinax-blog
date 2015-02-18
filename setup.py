551191b7fbcc31e910fbf8a1e59b04d3f6f44022
Patrick Altman <paltman@gmail.com>
2015-02-17 21:47:46 -0600

    Bump version for next release

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ URL = "https://github.com/pinax/pinax-blog"
 
 setup(
     name=NAME,
-    version="3.1.1",
+    version="3.1.2",
     description=DESCRIPTION,
     long_description=read("README.rst"),
     url=URL,
