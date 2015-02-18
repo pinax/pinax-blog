fd49529459d9ff3b7d1e14ad4dba50a7184272bb
Patrick Altman <paltman@gmail.com>
2015-02-17 21:56:16 -0600

    Fix migrations again

--- pinax/blog/migrations/0001_initial.py
+++ pinax/blog/migrations/0001_initial.py
@@ -8,6 +8,10 @@ from django.conf import settings
 
 class Migration(migrations.Migration):
 
+    dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+    ]
+
     operations = [
         migrations.CreateModel(
             name='FeedHit',
