f7ad5d25686dd7689f5e1e2b7edf713f0692a885
Patrick Altman <paltman@gmail.com>
2015-06-10 00:29:28 -0400

    Fix migration to add existing sections

--- pinax/blog/migrations/0003_auto_20150529_0405.py
+++ pinax/blog/migrations/0003_auto_20150529_0405.py
@@ -1,7 +1,15 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from django.conf import settings
 from django.db import models, migrations
+
+
+def seed_sections(apps, schema_editor):
+    Section = apps.get_model("blog", "Section")
+    db_alias = schema_editor.connection.alias
+    for section in settings.PINAX_BLOG_SECTIONS:
+        Section.objects.using(db_alias).create(slug=section[0], name=section[1])
 
 
 class Migration(migrations.Migration):
@@ -23,11 +31,8 @@ class Migration(migrations.Migration):
             },
             bases=(models.Model,),
         ),
-        migrations.AlterField(
-            model_name='post',
-            name='markup',
-            field=models.CharField(max_length=25, choices=[('markdown', 'Markdown')]),
-            preserve_default=True,
+        migrations.RunPython(
+            code=seed_sections,
         ),
         migrations.AlterField(
             model_name='post',
