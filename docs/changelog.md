5f66d0f212903304843b84b977741fc0b5d50e7c
Patrick Altman <paltman@gmail.com>
2015-06-10 01:21:08 -0400

    4.1.1

--- docs/changelog.md
+++ docs/changelog.md
@@ -1,5 +1,9 @@
 # Change Log
 
+## 4.1.1
+
+* Fix bug in how date based urls are generated without localizing time first
+
 ## 4.1.0
 
 * Added sections as model
--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ URL = "https://github.com/pinax/pinax-blog"
 
 setup(
     name=NAME,
-    version="4.1.0",
+    version="4.1.1",
     description=DESCRIPTION,
     long_description=read("README.rst"),
     url=URL,
