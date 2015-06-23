728775d18319eb2ba3f02c01c82f0b3b8b065a0d
Patrick Altman <paltman@gmail.com>
2015-06-22 23:32:26 -0500

    Single quotes must die in a fire

--- pinax/blog/urls.py
+++ pinax/blog/urls.py
@@ -12,11 +12,11 @@ from .views import (
 
 urlpatterns = patterns(
     "pinax.blog.views",
-    url(r'^$', BlogIndexView.as_view(), name="blog"),
-    url(r'^section/(?P<section>[-\w]+)/$', SectionIndexView.as_view(), name="blog_section"),
-    url(r'^(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/(?P<slug>[-\w]+)/$', DateBasedPostDetailView.as_view(), name="blog_post"),
-    url(r'^(?P<post_slug>[-\w]+)/$', SlugUniquePostDetailView.as_view(), name="blog_post_slug"),
-    url(r'^post/(?P<post_pk>\d+)/$', StaffPostDetailView.as_view(), name="blog_post_pk"),
-    url(r'^post/(?P<post_secret_key>\w+)/$', SecretKeyPostDetailView.as_view(), name="blog_post_secret"),
-    url(r'^feed/(?P<section>[-\w]+)/(?P<feed_type>[-\w]+)/$', "blog_feed", name="blog_feed"),
+    url(r"^$", BlogIndexView.as_view(), name="blog"),
+    url(r"^section/(?P<section>[-\w]+)/$", SectionIndexView.as_view(), name="blog_section"),
+    url(r"^(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/(?P<slug>[-\w]+)/$", DateBasedPostDetailView.as_view(), name="blog_post"),
+    url(r"^(?P<post_slug>[-\w]+)/$", SlugUniquePostDetailView.as_view(), name="blog_post_slug"),
+    url(r"^post/(?P<post_pk>\d+)/$", StaffPostDetailView.as_view(), name="blog_post_pk"),
+    url(r"^post/(?P<post_secret_key>\w+)/$", SecretKeyPostDetailView.as_view(), name="blog_post_secret"),
+    url(r"^feed/(?P<section>[-\w]+)/(?P<feed_type>[-\w]+)/$", "blog_feed", name="blog_feed"),
 )
