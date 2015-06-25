2c5c98a034bada90b8faf91d7d1ab7dabd7db6bb
Patrick Altman <paltman@gmail.com>
2015-06-24 20:47:51 -0500

    Do not include both post detail urls

--- pinax/blog/urls.py
+++ pinax/blog/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import url, patterns
 
+from .conf import settings
 from .views import (
     BlogIndexView,
     DateBasedPostDetailView,
@@ -14,9 +15,19 @@ urlpatterns = patterns(
     "pinax.blog.views",
     url(r"^$", BlogIndexView.as_view(), name="blog"),
     url(r"^section/(?P<section>[-\w]+)/$", SectionIndexView.as_view(), name="blog_section"),
-    url(r"^(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/(?P<slug>[-\w]+)/$", DateBasedPostDetailView.as_view(), name="blog_post"),
-    url(r"^(?P<post_slug>[-\w]+)/$", SlugUniquePostDetailView.as_view(), name="blog_post_slug"),
     url(r"^post/(?P<post_pk>\d+)/$", StaffPostDetailView.as_view(), name="blog_post_pk"),
     url(r"^post/(?P<post_secret_key>\w+)/$", SecretKeyPostDetailView.as_view(), name="blog_post_secret"),
     url(r"^feed/(?P<section>[-\w]+)/(?P<feed_type>[-\w]+)/$", "blog_feed", name="blog_feed"),
 )
+
+
+if settings.PINAX_BLOG_SLUG_UNIQUE:
+    urlpatterns += patterns(
+        "",
+        url(r"^(?P<post_slug>[-\w]+)/$", SlugUniquePostDetailView.as_view(), name="blog_post_slug")
+    )
+else:
+    urlpatterns += patterns(
+        "",
+        url(r"^(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/(?P<slug>[-\w]+)/$", DateBasedPostDetailView.as_view(), name="blog_post"),
+    )
