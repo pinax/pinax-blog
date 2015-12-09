150815f60028e0fb15ab586f62922d041457a5a1
Patrick Altman <paltman@gmail.com>
2015-12-09 15:00:55 -0600

    Update CI

--- setup.py
+++ setup.py
@@ -30,6 +30,13 @@ setup(
             "templates/pinax/blog/*.xml",
         ]
     },
+    tests_require=[
+        "django-appconf>=1.0.1",
+        "pytz==2015.7",
+        "Pillow>=3.0.0",
+        "Markdown>=2.6.5",
+        "Pygments>=2.0.2"
+    ],
     install_requires=[
         "django-appconf>=1.0.1",
         "pytz==2014.10",
