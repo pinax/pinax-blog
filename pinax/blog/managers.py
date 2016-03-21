7e04e5228975fa799575f9ed6a2ea33782dbe457
Patrick Altman <paltman@gmail.com>
2016-03-21 16:02:20 -0400

    Filter to only current published

--- pinax/blog/managers.py
+++ pinax/blog/managers.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 
 from .conf import settings
 
@@ -9,7 +10,7 @@ PUBLISHED_STATE = len(settings.PINAX_BLOG_UNPUBLISHED_STATES) + 1
 class PostManager(models.Manager):
 
     def published(self):
-        return self.filter(published__isnull=False, state=PUBLISHED_STATE)
+        return self.filter(published__lte=timezone.now(), state=PUBLISHED_STATE)
 
     def current(self):
         return self.published().order_by("-published")
