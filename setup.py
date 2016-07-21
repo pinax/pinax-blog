6c26adda7dae04af9e31da5e7c6172f8def3346f
James Tauber <jtauber@jtauber.com>
2016-07-21 10:53:48 -0400

    bumped version to 5.0.1

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ URL = "https://github.com/pinax/pinax-blog"
 
 setup(
     name=NAME,
-    version="5.0.0",
+    version="5.0.1",
     description=DESCRIPTION,
     long_description=read("README.rst"),
     url=URL,
