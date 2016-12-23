39a68500ee346e26f2b6f41b60bd03fced11dff0
Patrick Altman <paltman@gmail.com>
2016-12-22 21:37:46 -0500

    Use a better name

--- pinax/blog/context_processors.py
+++ pinax/blog/context_processors.py
@@ -3,5 +3,5 @@ from .conf import settings
 
 def scoped(request):
     return {
-        "scoped_var": request.resolver_match.kwargs.get(settings.PINAX_BLOG_SCOPING_URL_VAR)
+        "scoper_lookup": request.resolver_match.kwargs.get(settings.PINAX_BLOG_SCOPING_URL_VAR)
     }
