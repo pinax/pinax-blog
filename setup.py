d2ae858bedc771ce4dcda6fb0c37cf4d1cf36ea0
Graham Ullrich <graham@flyingcracker.com>
2018-01-09 17:50:24 -0700

    Major version bump for Django support update
    Adding Django 2.0 support, dropping <=1.10 as per Django project.

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import find_packages, setup
 
-VERSION = "6.4.0"
+VERSION = "7.0.0"
 LONG_DESCRIPTION = """
 .. image:: http://pinaxproject.com/pinax-design/patches/pinax-app.svg
     :target: https://pypi.python.org/pypi/pinax-app/
