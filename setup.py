bfe5b4ec9845c3e00b9a3a83b75b9294df275f6c
KatherineMichel <kthrnmichel@gmail.com>
2019-03-21 22:34:11 -0500

    Updating version number

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import find_packages, setup
 
-VERSION = "7.0.4-image-reenabled"
+VERSION = "7.0.5"
 LONG_DESCRIPTION = """
 .. image:: http://pinaxproject.com/pinax-design/patches/pinax-blog.svg
     :target: https://pypi.python.org/pypi/pinax-blog/
