67623a9b4a3c69fed72180cf2f61873b0a6a0239
Spencer Stecko <spencerstecko@gmail.com>
2021-02-04 06:35:30 -0800

    upgrade markdown to 3.1.1

--- setup.py
+++ setup.py
@@ -95,7 +95,7 @@ setup(
     install_requires=[
         "django>=2.2",
         "django-appconf>=1.0.1",
-        "markdown==2.6.11",
+        "markdown==3.1.1",
         "pillow>=3.0.0",
         "pinax-images>=3.0.1",
         "pygments>=2.0.2",
